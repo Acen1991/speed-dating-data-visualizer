eacd2fffc7392c059b35c9644f9f0e66c963da94
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-02-26 23:37:27 +0100

    first concrete visualization

--- client/js/app.js
+++ client/js/app.js
@@ -4,54 +4,84 @@ angular.module("speed-data-app", []).controller("baseController", ['$scope', fun
 /* some arguments AGAINST the use of dual-scaled axes line graphs can be found at http://www.perceptualedge.com/articles/visual_business_intelligence/dual-scaled_axes.pdf */
 
 // define dimensions of graph
-var m = [80, 80, 80, 80]; // margins
-var w = 1200 - m[1] - m[3]; // width
-var h = 500 - m[0] - m[2]; // height
 
-//Replace this with a backend call
-var data = {
-    "common": {
-        "m_attribute": "job",
-        "f_attribute": " wage",
-        "m_axis_values": ["lawyer", "doctor", "engineer", "artist"],
-        "f_axis_values": ["1K", "2K", "3K", "4K", "5K", "6K"]
-    },
-    "data": [{
-        "m_attribute_value": "doctor",
-        "f_attribute_value": "5K",
-        "m_success_attribute": 0.7,
-        "f_success_attribute": 0.5,
-        "m_like_f": 0.6,
-        "f_like_m": 0.3,
-        "both_like": 0.5,
-        "accuracy": 0.1
-    }]
-};
+d3.json("js/data.json", function(error,json){
+	 if (error) return console.warn(error);
+  	 vizualize(json);
+})
 
-var ym_range = d3.scale.ordinal().domain(data.common.m_axis_values).rangePoints([0, 400]);
-//for example to know where is the position of "engineer" on the axis we need to call ym_range.call('scale','engineer')
-yf_range = d3.scale.ordinal().domain(data.common.f_axis_values).rangePoints([0, 400]);
-//for example to know where is the position of    "2K"    on the axis we need to call yf_range.call('scale','2K')
+var vizualize = function(speed_dating_data){
+	var m = [80, 80, 80, 80]; // margins
+	var w = 1200 - m[1] - m[3]; // width
+	var h = 500 - m[0] - m[2]; // height
 
-// Add an SVG element with the desired dimensions and margin.
-var graph = d3.select("#graph").append("svg:svg")
-    .attr("width", w + m[1] + m[3])
-    .attr("height", h + m[0] + m[2])
-    .append("svg:g")
-    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+	var ym_range = d3.scale.ordinal().domain(speed_dating_data.attributes_data.m_axis_values).rangePoints([0, 400]);
+	//for example to know where is the position of "engineer" on the axis we need to call ym_range.call('scale','engineer')
+	yf_range = d3.scale.ordinal().domain(speed_dating_data.attributes_data.f_axis_values).rangePoints([0, 400]);
+	//for example to know where is the position of    "2K"    on the axis we need to call yf_range.call('scale','2K')
 
-// create left yAxis
-var ymAxis = d3.svg.axis().scale(ym_range).ticks(4).orient("left");
-// Add the y-axis to the left
-graph.append("svg:g")
-    .attr("class", "y axis axisLeft")
-    .attr("transform", "translate(-15,0)")
-    .call(ymAxis);
+	// Add an SVG element with the desired dimensions and margin.
+	var graph = d3.select("#graph")
+	    .append("g")
+	    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
-// create right yAxis
-var yfRight = d3.svg.axis().scale(yf_range).ticks(6).orient("right");
-// Add the y-axis to the right
-graph.append("svg:g")
-    .attr("class", "y axis axisRight")
-    .attr("transform", "translate(" + (w + 15) + ",0)")
-    .call(yfRight);
+	// create left yAxis
+	var ymAxis = d3.svg.axis().scale(ym_range).ticks(4).orient("left");
+	// Add the y-axis to the left
+	graph.append("g")
+	    .attr("class", "y axis axisLeft")
+	    .attr("transform", "translate(-15,0)")
+	    .call(ymAxis);
+
+	// create right yAxis
+	var yfAxis = d3.svg.axis().scale(yf_range).ticks(6).orient("right");
+	// Add the y-axis to the right
+	graph.append("g")
+	    .attr("class", "y axis axisRight")
+	    .attr("transform", "translate(" + (w + 15) + ",0)")
+	    .call(yfAxis);
+
+	graph.selectAll("g")
+		.data(speed_dating_data.values, function(d,i){
+			if(typeof d == "object"){
+				return d.couple_id;
+			} else {
+				return d;
+			}
+		})
+		.enter()
+			.append("g")
+			.append("polygon")
+				.attr("points",function(d,i){
+					var ymScaledPosition = ym_range.call('scale',d.m_attribute_value);
+					var yfScaledPosition = yf_range.call('scale',d.f_attribute_value);
+					var m_success_factor = d.m_success_attribute*25;
+					var f_success_factor = d.f_success_attribute*25;
+
+
+					var topLeftPoint = [-15, ymScaledPosition-m_success_factor];
+					var topRightPoint = [w+15 , yfScaledPosition-f_success_factor];
+					var bottomRightPoint = [w+15, yfScaledPosition+f_success_factor];
+					var bottomLeftPoint = [-15, ymScaledPosition+m_success_factor];
+					
+					var allPoints = [];
+					allPoints.push(topLeftPoint.join(','));
+					allPoints.push(topRightPoint.join(','));
+					allPoints.push(bottomRightPoint.join(','));
+					allPoints.push(bottomLeftPoint.join(','));
+				
+					return allPoints.join(' ');
+				})
+				.attr("style",function(d){
+					var fill = "fill:rgb("+[d3.round(255*d.m_like_f),d3.round(255*d.m_like_f),d3.round(255*d.m_like_f)].join(",")+")";
+					var strokeWidth="stroke-width:1";
+					var stroke = "stroke:purple";
+
+					var styles = [];
+					styles.push(fill);
+					styles.push(strokeWidth);
+					styles.push(stroke);
+
+					return styles.join(";");
+				});
+}
