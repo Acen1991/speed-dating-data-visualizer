041762b6e4fe48fd9c5f2ddbdf79a8ddfa4c9488
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-09-22 15:56:34 +0200

    refactoring watching attributes that the user selects

--- angular-nodejs/client/js/app.controller.js
+++ angular-nodejs/client/js/app.controller.js
@@ -1,28 +1,20 @@
-angular
-    .module("speed-data-app")
+angular.module("speed-data-app")
     .controller("baseController", ['$scope', 'vizualizeD3', '$http', function($scope, vizualizeD3, $http) {
-        $scope
-            .$watch('mAttribute', function(newValue, oldValue){
-            changeAttributeFunc(newValue, oldValue);
-        });
 
-        $scope
-            .$watch('wAttribute', function(newValue, oldValue){
-            changeAttributeFunc(newValue, oldValue);
-        });
-
-        var changeAttributeFunc = function(newValue, oldValue){
-            if(newValue != oldValue && $scope.mAttribute !== undefined && $scope.wAttribute !== undefined){
-                $http.get('data/'+$scope.mAttribute.normalized_attribute_name+'_'+$scope.wAttribute.normalized_attribute_name+'.json')
-                .success(function(data, status, headers, config){
-                    d3.select("#graph g").remove();
-                    vizualizeD3(data);
-                });
+        $scope.$watchCollection(function() {
+            return [$scope.mAttribute, $scope.wAttribute];
+        }, function() {
+            if ($scope.mAttribute !== undefined && $scope.wAttribute !== undefined) {
+                $http.get('data/' + $scope.mAttribute.normalized_attribute_name + '_' + $scope.wAttribute.normalized_attribute_name + '.json')
+                    .success(function(data) {
+                        d3.select("#graph g").remove();
+                        vizualizeD3(data);
+                    });
             }
-        };
-
-       $http.get('data/attributes.json')
-        .success(function(data, status, headers, config){
-            $scope.attributes = data;
         });
+
+        $http.get('data/attributes.json')
+            .success(function(data) {
+                $scope.attributes = data;
+            });
     }]);
