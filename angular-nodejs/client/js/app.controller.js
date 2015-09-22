a0adca18a9af1f17d036608e63fc546b2ce3cd31
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-09-22 16:16:37 +0200

    Update app.controller.js

--- angular-nodejs/client/js/app.controller.js
+++ angular-nodejs/client/js/app.controller.js
@@ -1,5 +1,5 @@
 angular.module("speed-data-app")
-    .controller("baseController", ['$scope', 'vizualizeD3', '$http', function($scope, vizualizeD3, $http) {
+    .controller("baseController", ['$scope', 'Vizualize', '$http', function($scope, Vizualize, $http) {
 
         $scope.$watchCollection(function() {
             return [$scope.mAttribute, $scope.wAttribute];
@@ -8,7 +8,7 @@ angular.module("speed-data-app")
                 $http.get('data/' + $scope.mAttribute.normalized_attribute_name + '_' + $scope.wAttribute.normalized_attribute_name + '.json')
                     .success(function(data) {
                         d3.select("#graph g").remove();
-                        vizualizeD3(data);
+                        Vizualize(data);
                     });
             }
         });
